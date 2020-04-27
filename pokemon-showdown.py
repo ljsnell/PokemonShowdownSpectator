d4dbdc72521c33f2f21a5ac033772de20a889416
ljsnell <ljsnell@users.noreply.github.com>
2020-04-27 17:12:52 -0500

    Forfeit fix (#8)
    * Fixed abandoned rooms, sort of
    
    * Moved to to generic function.
    
    * Added logic to randomly determine winner if the match goes longer than 20 min.
    
    Co-authored-by: Luke <luke@reetayn.com>

--- pokemon-showdown.py
+++ pokemon-showdown.py
@@ -8,6 +8,8 @@ from selenium import webdriver
 import time
 from get_names import getNames, getWinner
 from twitch_connect import twitch_chat_bot
+from close_battle import closeBattle
+import random
 
 # start twitch_chat_bot - credentials/channel specified in `twitch_connect.py`
 twitch_handler = twitch_chat_bot()
@@ -46,42 +48,39 @@ while True:
     # extract player names from battle log
     battle_log = driver.find_element_by_class_name("battle-log").text
     left_name, right_name = getNames(battle_log)
-    
     # Post bet & options to twitch chat
     twitch_handler.post_msg('!bet open "Who will win?" "' + left_name + ", " + right_name + '" 1 1000 2')
     
     # loop until battle ends
+    loop_count = 0
     battle_over, bets_open = False, True
     while not battle_over:
-        
+        loop_count = loop_count + 1
         # wait twenty seconds
         time.sleep(20)
         battle_log = driver.find_element_by_class_name("battle-log").text
         
         # report winner and close bets to Twitch Channel
-        if ("won the battle!" or "This room is expired" or "All players are inactive." or "Tie between") in battle_log:
+        if "won the battle!" in battle_log:
             battle_over = True
             winner = getWinner(battle_log)
-            print(battle_log)
             print("winner")
             print(winner)
+            twitch_handler.post_msg("!bet close " + winner)            
+            closeBattle(driver)
+
+        # If > 20 minutes have passed for a battle abandon it.
+        elif loop_count > 60:
+            battle_over = True
+            # Randomly determine winner
+            winner = random.choice([left_name, right_name])            
+            twitch_handler.post_msg("Room timed out, defaulting winner.")
             twitch_handler.post_msg("!bet close " + winner)
             
-            # 10 seconds after bets close, exit battle and loop
-            time.sleep(10)
-            attempts = 0
-            while attempts < 3:
-                try:
-                    close_battle_button = driver.find_element_by_name("closeRoom")
-                    close_battle_button.click()
-                    print("Close button clicked")
-                    break
-                except:
-                    print("Caught stale exception")
-                    driver.refresh()
-                attempts = attempts + 1
-
+            closeBattle(driver)
+            
     # return to and refresh battle list
+    print(battle_log)
     time.sleep(5)
     view_battle_button = driver.find_element_by_xpath('//*[@id="room-"]/div/div[1]/div[2]/div[3]/p[1]/button')
     view_battle_button.click()
