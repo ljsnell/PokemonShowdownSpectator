49e0ae2a8caa44ced11f14a744f75b9b0e256175
Jordan Gunn <gunnjordanb@gmail.com>
2020-04-26 23:49:51 -0500

    Code comments

--- twitch_connect.py
+++ twitch_connect.py
@@ -7,12 +7,15 @@ import time
 from dotenv import load_dotenv
 load_dotenv()
 
-class twitch_chat_bot:    
+class twitch_chat_bot:
+    
+    # connection information
     connection_data = ('irc.chat.twitch.tv', 6667)
     token = os.getenv('TOKEN')
     user_name = 'python_bot'
     channel = '#showdownspectator'
-    readbuffer = ''
+
+    # initiate connection
     server = socket.socket()    
     server.connect(connection_data)
     server.send(bytes('PASS '+ token + '\r\n', 'utf-8'))
@@ -20,6 +23,8 @@ class twitch_chat_bot:
     server.send(bytes('JOIN '+ channel + '\r\n', 'utf-8'))
 
     def post_msg(self, text_to_send):
+        "Attempt to post message to channel, restarting server connection upon first failure."
+        
         try:
             msg = "PRIVMSG " + self.channel + " :" + text_to_send + "\r\n"
             self.server.send(bytes(msg, 'utf-8'))
