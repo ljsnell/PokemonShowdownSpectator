2a7bfd7b47c0a663bfd7c3beb8063923dd03cfa7
ljsnell <ljsnell7@gmail.com>
2020-04-26 22:07:53 -0500

    Updated to clean string to be safe.

--- get_names.py
+++ get_names.py
@@ -1,13 +1,18 @@
+import re
+
 def getNames(loaded_text):
     split_text = loaded_text.split("\n")
-    left_name = split_text[1].replace(' joined.', '').replace('☆', '')
-    right_name = split_text[2].replace(' joined.', '').replace('☆', '')
+    left_name = getCleanString(split_text[1].replace(' joined.', '').replace('☆', ''))
+    
+    right_name = getCleanString(split_text[2].replace(' joined.', '').replace('☆', ''))
     return left_name, right_name
 
 def getWinner(bodyText):
     matched_lines = [line for line in bodyText.split('\n') if " won " in line]
     # Find first part and return slice before it.
     pos_a = matched_lines[0].find(' won ')
-    print(pos_a)
-    return matched_lines[0][0:pos_a]
+    winner = getCleanString(matched_lines[0][0:pos_a])
+    return winner
     
+def getCleanString(string2Clean):
+    return re.sub(r'\W+', '', string2Clean)
